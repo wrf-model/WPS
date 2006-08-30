89df1469cbf676f3ed9aece4c299e33c59bca506
Michael Duda <duda@ucar.edu>
2006-08-30 20:02:28 +0000

    Added avt_tsfc utility to compute daily average surface temperature.
    git-svn-id: https://svn-wrf-wps.cgd.ucar.edu/trunk@118 86b71a92-4018-0410-97f8-d555beccfc3a

---
+++ metgrid/src/avg_tsfc.F90
@@ -0,0 +1,120 @@
+program avg_tsfc
+
+   use date_pack
+   use gridinfo_module
+   use read_met_module
+   use write_met_module
+   use module_debug
+
+   implicit none
+
+   ! Local variables
+   integer :: idiff, n_times, t, istatus, fg_idx, discardtimes
+   integer :: met_map_proj, version, nx, ny
+   real :: xfcst, xlvl, startlat, startlon, starti, startj, deltalat, deltalon
+   real :: met_dx, met_dy
+   real :: met_cen_lon, met_truelat1, met_truelat2
+   real, pointer, dimension(:,:) :: slab
+   real, pointer, dimension(:,:) :: mean
+   logical :: is_rotated
+   character (len=9) :: short_fieldnm
+   character (len=19) :: valid_date, temp_date
+   character (len=24) :: hdate
+   character (len=25) :: units
+   character (len=32) :: map_src
+   character (len=46) :: desc
+   character (len=128) :: input_name
+
+   call get_namelist_params()
+
+   ! Compute number of times that we will process
+   call geth_idts(end_date(1), start_date(1), idiff)
+   call mprintf((idiff < 0),ERROR,'Ending date is earlier than starting date in namelist for domain %i.', i1=1)
+
+   n_times = idiff / interval_seconds
+
+   ! Check that the interval evenly divides the range of times to process
+   call mprintf((mod(idiff, interval_seconds) /= 0),WARN, &
+                'In namelist, interval_seconds does not evenly divide '// &
+                '(end_date - start_date) for domain %i. Only %i time periods '// &
+                'will be processed.', i1=1, i2=n_times)
+
+   fg_idx = 1
+
+   input_name = fg_name(fg_idx)
+
+   discardtimes = mod(idiff+interval_seconds,86400) / interval_seconds
+
+   do while (input_name /= '*')
+
+      ! Loop over all times to be processed for this domain
+      do t=0,n_times-discardtimes
+
+         call geth_newdate(valid_date, trim(start_date(1)), t*interval_seconds)
+         temp_date = ' '
+         write(temp_date,'(a19)') valid_date(1:10)//'_'//valid_date(12:19)
+
+         ! Initialize the module for reading in the met fields
+         call read_met_init(trim(input_name), .false., temp_date(1:13), istatus)
+
+         if (istatus == 0) then
+write(6,*) 'Reading from '//trim(input_name), ' at time ', temp_date(1:13)
+
+            ! Process all fields and levels from the current file; read_next_met_field()
+            !   will return a non-zero status when there are no more fields to be read.
+            do while (istatus == 0)
+
+
+               call read_next_met_field(version, short_fieldnm, hdate, xfcst, xlvl, units, desc, &
+                                   met_map_proj, startlat, startlon, starti, startj, deltalat, &
+                                   deltalon, met_dx, met_dy, met_cen_lon, met_truelat1, met_truelat2, nx, ny, &
+                                   map_src, slab, is_rotated, istatus)
+
+               if (istatus == 0) then
+
+                  if (trim(short_fieldnm) == 'TT' .and. xlvl == 200100.) then
+! BUG: Should check here whether projection and dimensions match from previous read of the TT field
+                     if (.not. allocated(mean)) then
+                        allocate(mean(nx,ny))
+                        mean = 0.
+                     end if
+                     mean = mean + slab
+                  end if
+   
+                  if (associated(slab)) deallocate(slab)
+   
+               end if
+   
+            end do
+   
+            call read_met_close()
+
+         else
+write(6,*) 'Problem opening '//trim(input_name), ' at time ', temp_date(1:13)
+stop
+         end if
+
+      end do 
+
+      if (associated(mean)) then
+         mean = mean /real(n_times+1)
+
+         call write_met_init('TAVGSFC', .true., temp_date(1:13), istatus)
+! BUG: Need to save all of these arguments from the TT field read
+         call write_next_met_field(version, 'TAVGSFC  ', hdate, xfcst, 200100., units, desc, &
+                             met_map_proj, startlat, startlon, starti, startj, deltalat, &
+                             deltalon, met_dx, met_dy, met_cen_lon, met_truelat1, met_truelat2, nx, ny, &
+                             map_src, mean, is_rotated, istatus)
+         call write_met_close()
+  
+         deallocate(mean)
+      end if
+
+      fg_idx = fg_idx + 1
+      input_name = fg_name(fg_idx)
+
+   end do 
+
+   stop
+
+end program avg_tsfc
---
+++ metgrid/src/write_met_module.f90
@@ -0,0 +1,196 @@
+module write_met_module
+
+   use module_debug
+   use misc_definitions_module
+
+   ! State variables?
+   integer :: output_unit
+   character (len=128) :: met_out_filename
+ 
+   contains
+ 
+   subroutine write_met_init(fg_source, source_is_constant, datestr, istatus)
+ 
+      implicit none
+  
+      ! Arguments
+      integer, intent(out) :: istatus
+      logical, intent(in) :: source_is_constant
+      character (len=*) :: fg_source
+      character (len=*) :: datestr
+  
+      ! Local variables
+      integer :: io_status
+      logical :: is_used
+
+      istatus = 0
+    
+      !  1) BUILD FILENAME BASED ON TIME 
+      met_out_filename = ' '
+      if (.not. source_is_constant) then 
+         write(met_out_filename, '(a)') trim(fg_source)//':'//trim(datestr)
+      else
+         write(met_out_filename, '(a)') trim(fg_source)
+      end if
+  
+      !  2) OPEN FILE
+      do output_unit=10,100
+         inquire(unit=output_unit, opened=is_used)
+         if (.not. is_used) exit
+      end do 
+      call mprintf((output_unit > 100),ERROR,'In write_met_init(), couldn''t find an available Fortran unit.')
+      open(unit=output_unit, file=trim(met_out_filename), status='unknown', form='unformatted', iostat=io_status)
+
+      if (io_status > 0) istatus = 1
+
+      return
+  
+ 
+   end subroutine write_met_init
+ 
+ 
+   subroutine write_next_met_field(version, field, hdate, xfcst, xlvl, units, desc, &
+                          iproj, startlat, startlon, starti, startj, deltalat, &
+                          deltalon, dx, dy, xlonc, truelat1, truelat2, nx, ny, map_source, &
+                          slab, is_rotated_windfield, istatus)
+ 
+      implicit none
+  
+      ! Arguments
+      integer, intent(in) :: version, nx, ny, iproj
+      integer, intent(out) :: istatus
+      real, intent(in) :: xfcst, xlvl, startlat, startlon, starti, startj, &
+                           deltalat, deltalon, dx, dy, xlonc, truelat1, truelat2
+      real, dimension(nx,ny) :: slab
+      logical, intent(in) :: is_rotated_windfield
+      character (len=9), intent(in) :: field
+      character (len=24), intent(in) :: hdate
+      character (len=25), intent(in) :: units
+      character (len=32), intent(in) :: map_source
+      character (len=46), intent(in) :: desc
+  
+      ! Local variables
+      character (len=8) :: startloc
+  
+      istatus = 1
+  
+      !  1) WRITE FORMAT VERSION
+      write(unit=output_unit) version
+
+      ! PREGRID
+      if (version == 3) then
+
+         ! Cylindrical equidistant
+         if (iproj == PROJ_LATLON) then
+            write(unit=output_unit) hdate, xfcst, field, units, desc, xlvl, nx, ny, 0
+            write(unit=output_unit) startlat, startlon, deltalat, deltalon
+     
+         ! Mercator
+         else if (iproj == PROJ_MERC) then
+            write(unit=output_unit) hdate, xfcst, field, units, desc, xlvl, nx, ny, 1
+            write(unit=output_unit) startlat, startlon, dx, dy, truelat1
+     
+         ! Lambert conformal
+         else if (iproj == PROJ_LC) then
+            write(unit=output_unit) hdate, xfcst, field, units, desc, xlvl, nx, ny, 3
+            write(unit=output_unit) startlat, startlon, dx, dy, xlonc, truelat1, truelat2
+     
+         ! Polar stereographic
+         else if (iproj == PROJ_PS) then
+            write(unit=output_unit) hdate, xfcst, field, units, desc, xlvl, nx, ny, 5
+            write(unit=output_unit) startlat, startlon, dx, dy, xlonc, truelat1
+
+         ! ?????????
+         else
+     
+         end if
+     
+         write(unit=output_unit) slab
+     
+         istatus = 0 
+    
+      ! GRIB_PREP
+      else if (version == 4) then
+
+         if (starti == 1.0 .and. startj == 1.0) then
+            startloc='SWCORNER'
+         else
+            startloc='CENTER  '
+         end if
+  
+         ! Cylindrical equidistant
+         if (iproj == PROJ_LATLON) then
+            write(unit=output_unit) hdate, xfcst, map_source, field, units, desc, xlvl, nx, ny, 0
+            write(unit=output_unit) startloc, startlat, startlon, deltalat, deltalon
+
+         ! Lambert conformal
+         else if (iproj == PROJ_LC) then
+            write(unit=output_unit) hdate, xfcst, map_source, field, units, desc, xlvl, nx, ny, 3
+            write(unit=output_unit) startloc, startlat, startlon, dx, dy, xlonc, truelat1, truelat2
+
+         ! Polar stereographic
+         else if (iproj == PROJ_PS) then
+            write(unit=output_unit) hdate, xfcst, map_source, field, units, desc, xlvl, nx, ny, 5
+            write(unit=output_unit) startloc, startlat, startlon, dx, dy, xlonc, truelat1
+     
+         ! ?????????
+         else
+     
+         end if
+  
+         write(unit=output_unit) slab
+      
+         istatus = 0
+
+      ! WPS
+      else if (version == 5) then
+
+         if (starti == 1.0 .and. startj == 1.0) then
+            startloc='SWCORNER'
+         else
+            startloc='CENTER  '
+         end if
+  
+         ! Cylindrical equidistant
+         if (iproj == PROJ_LATLON) then
+            write(unit=output_unit) hdate, xfcst, map_source, field, units, desc, xlvl, nx, ny, 0
+            write(unit=output_unit) startloc, startlat, startlon, deltalat, deltalon
+
+         ! Lambert conformal
+         else if (iproj == PROJ_LC) then
+            write(unit=output_unit) hdate, xfcst, map_source, field, units, desc, xlvl, nx, ny, 3
+            write(unit=output_unit) startloc, startlat, startlon, dx, dy, xlonc, truelat1, truelat2
+
+         ! Polar stereographic
+         else if (iproj == PROJ_PS) then
+            write(unit=output_unit) hdate, xfcst, map_source, field, units, desc, xlvl, nx, ny, 5
+            write(unit=output_unit) startloc, startlat, startlon, dx, dy, xlonc, truelat1
+     
+         ! ?????????
+         else
+     
+         end if
+  
+         write(unit=output_unit) slab
+      
+         istatus = 0
+
+      else
+         call mprintf(.true.,ERROR,'Didn''t recognize format number %i.', i1=version)
+      end if
+  
+      return
+ 
+   end subroutine write_next_met_field
+ 
+ 
+   subroutine write_met_close()
+ 
+      implicit none
+  
+      close(unit=output_unit)
+      met_out_filename = 'UNINITIALIZED_FILENAME'
+  
+   end subroutine write_met_close
+
+end module write_met_module
