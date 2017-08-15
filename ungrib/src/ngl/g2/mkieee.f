c367db5bc1c440dc9d406899aa36fc6015f76c8b
Michael Duda <duda@ucar.edu>
2017-08-15 15:35:52 -0600

    Add original lines of code to mkieee.f and rdieee.f with explanatory comment

--- ungrib/src/ngl/g2/mkieee.f
+++ ungrib/src/ngl/g2/mkieee.f
@@ -32,6 +32,11 @@
 
       integer(4) :: ieee 
 
+! Recent versions of the PGI compilers apparently still do not fully support
+! the use of all intrinsics in parameter statements, though this is part of
+! the F2003 standard.
+!      real, parameter :: two23=scale(1.0,23)
+!      real, parameter :: two126=scale(1.0,126)
       real :: two23
       real :: two126
 
