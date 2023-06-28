1dbee3acfeb63b57814e833e80256723edf58c4f
Anthony Islas <aislas@ucar.edu>
2023-06-28 17:37:16 -0600

    Copying files from WRF build, unfortunately

---
+++ cmake/modules/FindnetCDF.cmake
@@ -0,0 +1,71 @@
+# Find netcdf
+# Eventually replace with netCDF's actual config if using that
+# Once found this file will define:
+#  netCDF_FOUND - System has netcdf
+#  netCDF_INCLUDE_DIRS - The netcdf include directories
+#  netCDF_LIBRARIES - The libraries needed to use netcdf
+#  netCDF_DEFINITIONS - Compiler switches required for using netcdf
+
+# list( REMOVE_ITEM CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} )
+# find_package( netCDF )
+# list( APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} )
+
+# Use nc-config
+find_program( 
+                NETCDF_PROGRAM
+                nc-config
+                QUIET
+                )
+
+if ( ${NETCDF_PROGRAM} MATCHES "-NOTFOUND$" )
+  message( STATUS "No nc-config found" )
+else()
+  message( STATUS "Found NETCDF_PROGRAM : ${NETCDF_PROGRAM}" )
+
+  execute_process( COMMAND ${NETCDF_PROGRAM} --includedir   OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_INCLUDE_DIR )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --libdir       OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_LIBRARY_DIR )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --prefix       OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_PREFIX )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --libs         OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_CLIBS   )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --cxx4libs     OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_CXXLIBS )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --flibs        OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_FLIBS   )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --version      OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_VERSION_RAW )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --has-nc4      OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_NC4_YES )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --has-pnetcdf  OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_PNETCDF_YES )
+  execute_process( COMMAND ${NETCDF_PROGRAM} --has-parallel OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE netCDF_PARALLEL_YES )
+
+
+  # Sanitize version
+  string( REPLACE " " ";" netCDF_VERSION_LIST ${netCDF_VERSION_RAW} )
+  list( GET netCDF_VERSION_LIST -1 netCDF_VERSION )
+
+  # Convert to YES/NO - Note cannot be generator expression if you want to use it during configuration time
+  string( TOUPPER ${netCDF_NC4_YES}      netCDF_NC4      )
+  string( TOUPPER ${netCDF_PNETCDF_YES}  netCDF_PNETCDF  )
+  string( TOUPPER ${netCDF_PARALLEL_YES} netCDF_PARALLEL )
+
+  set( netCDF_DEFINITIONS  )
+
+  set( netCDF_LIBRARIES
+      $<$<LINK_LANGUAGE:C>:${netCDF_CLIBS}>
+      $<$<LINK_LANGUAGE:CXX>:${netCDF_CXXLIBS}> 
+      $<$<LINK_LANGUAGE:Fortran>:${netCDF_FLIBS}>
+      )
+  set( netCDF_INCLUDE_DIRS ${netCDF_INCLUDE_DIR} )
+endif()
+
+find_package( PkgConfig )
+
+include(FindPackageHandleStandardArgs)
+
+# handle the QUIETLY and REQUIRED arguments and set netCDF_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args( netCDF  DEFAULT_MSG
+                                  netCDF_INCLUDE_DIRS
+                                  netCDF_LIBRARY_DIR
+                                  netCDF_CLIBS
+                                  netCDF_CXXLIBS
+                                  netCDF_FLIBS
+                                  netCDF_VERSION
+                                  )
+
+mark_as_advanced( netCDF_CLIBS netCDF_CXXLIBS netCDF_FLIBS netCDF_PREFIX netCDF_LIBRARY_DIR )
---
+++ cmake/template/arch_config.cmake
@@ -0,0 +1,20 @@
+# https://cmake.org/cmake/help/latest/module/FindMPI.html#variables-for-locating-mpi
+set( MPI_Fortran_COMPILER "{DM_FC}" )
+set( MPI_C_COMPILER       "{DM_CC}" )
+
+set( CMAKE_Fortran_COMPILER "{SFC}" )
+set( CMAKE_C_COMPILER       "{SCC}" )
+
+set( CMAKE_Fortran_FLAGS_INIT    "{FFLAGS}" )
+set( CMAKE_C_FLAGS_INIT          "{CFLAGS}" )
+
+# set( CMAKE_Fortran_FLAGS_DEBUG_INIT    "{{FCDEBUG}}" )
+# set( CMAKE_Fortran_FLAGS_RELEASE_INIT  "{{FCOPTIM}}" )
+
+# Project specifics now
+set( WPS_MPI_Fortran_FLAGS  "{DM_FC_FLAGS}" )
+set( WPS_MPI_C_FLAGS        "{DM_CC_FLAGS}" )
+set( WPS_DEFINITIONS        "{CPPFLAGS}"    )
+# set( WPS_M4_FLAGS           "{{M4_FLAGS}}"    )
+# set( WPS_FCOPTIM            "{{FCOPTIM}}"     )
+# set( WPS_FCNOOPT            "{{FCNOOPT}}"     )
