5cbc43198ebae70bc62b564c7bb0d9133b5fd94d
Jim Bresch <bresch@ucar.edu>
2025-04-15 13:13:07 -0600

    Update to process ECMWF IFS soil level 151 fields.

--- ungrib/src/ngl/g2/params.f
+++ ungrib/src/ngl/g2/params.f
@@ -40,7 +40,7 @@
 !
 !$$$
 
-      integer,parameter :: MAXPARAM=816
+      integer,parameter :: MAXPARAM=842
 
       type gribparam
           integer :: g1tblver
@@ -884,6 +884,35 @@
        data paramlist(814) /gribparam(2,255,0,6,18,'TCOLWO')/
        data paramlist(815) /gribparam(2,255,0,6,19,'TCOLIO')/
        data paramlist(816) /gribparam(2,255,0,16,201,'RADARVIL')/
+! Added 06/05/24  for NCEP GFS
+       data paramlist(817) /gribparam(2,214,0,1,37,'CPRAT')/
+       data paramlist(818) /gribparam(2,255,10,2,8,'ICETMP')/
+! Added 06/05/24  for CMA GFS
+       data paramlist(819) /gribparam(2,255,2,0,24,'HFLUX')/
+       data paramlist(820) /gribparam(2,255,0,5,5,'NLWRF')/
+       data paramlist(821) /gribparam(2,255,0,4,9,'NSWRF')/
+       data paramlist(822) /gribparam(2,255,0,5,8,'DLWRFCS')/
+       data paramlist(823) /gribparam(2,255,0,4,11,'NSWRFCS')/
+       data paramlist(824) /gribparam(2,255,0,4,53,'USWRFCS')/
+       data paramlist(825) /gribparam(2,255,0,1,64,'TCIWV')/
+       data paramlist(826) /gribparam(2,255,0,1,69,'TCOLW')/
+       data paramlist(827) /gribparam(2,255,0,1,70,'TCOLI')/
+       data paramlist(828) /gribparam(2,255,0,6,22,'CDCC')/
+       data paramlist(829) /gribparam(2,136,0,2,25,'VWSH')/
+       data paramlist(830) /gribparam(2,255,0,1,19,'PTYPE')/
+       data paramlist(831) /gribparam(2,255,0,6,11,'CDCB')/
+       data paramlist(832) /gribparam(2,255,0,6,12,'CDCTOP')/
+! Added 04/14/25  for ECMWF IFS
+       data paramlist(833) /gribparam(2,255,2,3,18,'SOT  ')/
+       data paramlist(834) /gribparam(2,255,2,0,25,'VSW  ')/
+       data paramlist(835) /gribparam(2,255,192,128,39,'SWVL1')/
+       data paramlist(836) /gribparam(2,255,192,128,40,'SWVL2')/
+       data paramlist(837) /gribparam(2,255,192,128,41,'SWVL3')/
+       data paramlist(838) /gribparam(2,255,192,128,42,'SWVL4')/
+       data paramlist(839) /gribparam(2,255,192,128,139,'STL1 ')/
+       data paramlist(840) /gribparam(2,255,192,128,170,'STL2 ')/
+       data paramlist(841) /gribparam(2,255,192,128,183,'STL3 ')/
+       data paramlist(842) /gribparam(2,255,192,128,236,'STL4 ')/
 
       contains
 
