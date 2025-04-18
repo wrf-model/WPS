3e9f90bd6f3f3b35bd0270fddb9c0714788cd232
Anthony Islas <aislas@ucar.edu>
2025-04-17 17:25:58 -0700

    Use faster evaluation of tile points

--- util/TileData.py
+++ util/TileData.py
@@ -56,7 +56,7 @@ class TileData:
 
     if tile_j < 0 or tile_j > self.ny_:
       # Flip at the poles so return a view of the data rotated 180
-      true_tile_i = ( tile_i + self.nx_ / 2 ) % self.nx_
+      true_tile_i = int( tile_i + self.nx_ / 2 ) % self.nx_
       true_tile_j = self.ny_ - ( tile_j % ( self.ny_ + 1 ) )
       rotate      = True
 
@@ -71,35 +71,50 @@ class TileData:
 
   
     if rotate:
+      print( f"Tile at {tile_i} {tile_j} must be rotated" )
       data = np.rot90( data, 2 )
 
     return data
 
   def get_box( self, indices ):
-    print( np.einsum( "ijk, i -> ijk", indices, [ 1/self.tile_y_, 1/self.tile_x_ ] )  )
+
     # First transform indices to regions of distinct tiles by dividing the x and y 
     # indices by the respective tile sizes
     as_tile_idx = np.floor( np.einsum( "ijk, i -> ijk", indices, [ 1/self.tile_y_, 1/self.tile_x_ ] ) ).astype( int )
+
     # Create our unique set of tiles needed and load them into an analogous structure
-    uniq_tiles      = np.unique( np.unique( as_tile_idx, axis=1 ), axis=2 ).astype( int )
+    # Naive creation by checking for uniqueness
+    # uniq_tiles      = np.unique( np.unique( as_tile_idx, axis=1 ), axis=2 ).astype( int )
+    # We know they will increase monotonically so just generate the range
+    uniq_tiles = np.indices( ( as_tile_idx[0,-1,0] - as_tile_idx[0,0,0] + 1, as_tile_idx[1,0,-1] - as_tile_idx[1,0,0] + 1 ) )
+    uniq_tiles[0] += as_tile_idx[0,0,0]
+    uniq_tiles[1] += as_tile_idx[1,0,0]
+
     uniq_tiles_list = list( map( tuple, uniq_tiles.transpose( 1, 2, 0 ).reshape( int( uniq_tiles.size / 2 ), 2 ) ) )
-    print( uniq_tiles_list )
+
     tiles       = {}
     for tile in uniq_tiles_list:
-      print( f"Getting data for tile {tile}" )
       tiles[tile] = self.get_tile_by_index( tile[1], tile[0] )
     
     box = np.zeros( indices[0].shape, dtype=next(iter(tiles.values())).dtype )
     # Now we have our tile set easily accessible based on the 1-to-1 mapping of indices to tile idx
     
-    for j in range( indices.shape[1] ):
-      for i in range( indices.shape[2] ):
-        reli = indices[1,j,i] % self.tile_x_
-        relj = indices[0,j,i] % self.tile_y_
-        # print( f"i : {i} j : {j}")
-        # print( f"reli : {reli} relj : {relj}" )
-        # print( tiles[tuple(as_tile_idx[:,j,i])] )
-        box[j,i] = tiles[tuple(as_tile_idx[:,j,i])][relj,reli]
+    for tile in uniq_tiles_list:
+      print( f"Processing tile {tile}")
+      # Get the box indices corresponding to this tile
+      box_indices = np.logical_and( as_tile_idx[0] == tile[0], as_tile_idx[1] == tile[1] )
+
+      # Bulk assign to box this region of uniq tile by getting the relative index 
+      # into that tile from original indices
+      box[box_indices] = tiles[tile][indices[0,box_indices] % self.tile_y_, indices[1,box_indices] % self.tile_x_]
+
+    # Naively assign one-by-one
+    # for j in range( indices.shape[1] ):
+    #   for i in range( indices.shape[2] ):
+    #     reli = indices[1,j,i] % self.tile_x_
+    #     relj = indices[0,j,i] % self.tile_y_
+    #     box[j,i] = tiles[tuple(as_tile_idx[:,j,i])][relj,reli]
 
     self.print_present_tile_grid()
+
     return box
